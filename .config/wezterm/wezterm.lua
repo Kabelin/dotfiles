c94ee1527f2fbf664e0b9ef70cf2cd6eecfab460
Kabelin <vitor.cruvinel@umctecnologia.com>
2023-01-25 20:48:46 -0300

    :sparkles: add wezterm config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,15 @@
+local wezterm = require("wezterm")
+
+return {
+	color_scheme = "Catppuccin Mocha",
+	font = wezterm.font("FiraCode Nerd Font"),
+	use_fancy_tab_bar = false,
+	hide_tab_bar_if_only_one_tab = true,
+	tab_bar_at_bottom = true,
+	window_padding = {
+		left = 0,
+		right = 0,
+		top = 0,
+		bottom = 0,
+	},
+}
